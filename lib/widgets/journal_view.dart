1031f259f2ea230d935da3b12b317f00ca550a7a
Andre Paul <andrepaul8128@gmail.com>
2021-02-24 13:35:41 -0800

    boilerplate up for new widget

---
+++ lib/widgets/journal_view.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+import 'package:journal/models/journal.dart';
+
+class JournalView extends StatelessWidget {
+  static final routeName = "journal";
+
+  final journal = Journal();
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold();
+  }
+}
