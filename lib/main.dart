9d742cdd58b47c57245bcd34f9301564d482023d
Andre Paul <andrepaul8128@gmail.com>
2021-02-24 14:21:40 -0800

    use dynamic theme

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:dynamic_theme/dynamic_theme.dart';
 import 'package:journal/widgets/journal_entry_form.dart';
 import 'package:journal/widgets/my_home_page.dart';
 import 'package:journal/widgets/journal_view.dart';
@@ -21,20 +22,30 @@ class MyApp extends StatefulWidget {
 class _MyAppState extends State<MyApp> {
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Journal App',
-      // took this theme from flutter docs
-      theme: ThemeData(
-          brightness: Brightness.light,
-          primaryColor: Colors.purple,
-          accentColor: Colors.grey,
-          fontFamily: 'Georgia',
-          textTheme: TextTheme(
-            headline1: TextStyle(fontSize: 24.0, fontWeight: FontWeight.bold),
-            headline2: TextStyle(fontSize: 18.0, fontStyle: FontStyle.italic),
-            bodyText2: TextStyle(fontSize: 14.0, fontFamily: 'Hind'),
-          )),
-      home: MyHomePage(title: 'Journal'),
-    );
+    return DynamicTheme(
+        defaultBrightness: Brightness.light,
+        data: (brightness) => ThemeData(
+              primarySwatch: Colors.purple,
+              brightness: brightness,
+            ),
+        themedWidgetBuilder: (context, theme) {
+          return MaterialApp(
+            title: 'Journal App',
+            // took this theme from flutter docs
+            theme: ThemeData(
+                brightness: Brightness.light,
+                primaryColor: Colors.purple,
+                accentColor: Colors.grey,
+                fontFamily: 'Georgia',
+                textTheme: TextTheme(
+                  headline1:
+                      TextStyle(fontSize: 24.0, fontWeight: FontWeight.bold),
+                  headline2:
+                      TextStyle(fontSize: 18.0, fontStyle: FontStyle.italic),
+                  bodyText2: TextStyle(fontSize: 14.0, fontFamily: 'Hind'),
+                )),
+            home: MyHomePage(title: 'Journal'),
+          );
+        });
   }
 }
